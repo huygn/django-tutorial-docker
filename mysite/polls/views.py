3008d484ea8fa97e445f4f0276494a828e196b25
Huy Giang <gnhuy91@gmail.com>
2016-03-29 14:18:14 +0700

    finished tutorial#4 skipped generic Class-Based Views - kinda hard to reason about as compared to function view.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from django.http import Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.core.urlresolvers import reverse
+from django.db.models import F
 
-from .models import Question
+from .models import Question, Choice
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # latest_question_list = Question.objects.order_by(F('pub_date').desc())[:5]
     context = dict(latest_question_list=latest_question_list)
     return render(request, 'polls/index.html', context=context)
 
@@ -16,8 +18,29 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', context=context)
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = dict(question=question)
+    return render(request, 'polls/results.html', context=context)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        # selected_choice.votes += 1
+        # Use F() class to avoid race condition
+        # https://docs.djangoproject.com/en/1.9/ref/models/expressions/#avoiding-race-conditions-using-f
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        # contruct results url using namespace
+        results_url = reverse('polls:results', args=(question.id,))
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(results_url)
