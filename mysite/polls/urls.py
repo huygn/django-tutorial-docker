4c271d3ca1c4d611c34187efa7a321541474da51
Huy Giang <gnhuy91@gmail.com>
2016-03-31 14:34:07 +0700

    disable caching for now, until I have gone thru all tutorials

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,10 +1,13 @@
+from django.views.decorators.cache import never_cache, cache_page
 from django.conf.urls import url
 from . import views
+
 
 app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
     url(r'^$', views.index, name='index'),
+    # url(r'^$', cache_page(30)(views.index), name='index'),  # experiment caching
     # ex: /polls/5/
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     # ex: /polls/5/results/
