83e58bbef1c770b9fc4345c86aeb99dc14f5642f
Huy Giang <gnhuy91@gmail.com>
2016-04-02 00:04:05 +0700

    modified comments

--- apps/mysite/config/local.py
+++ apps/mysite/config/local.py
@@ -8,7 +8,7 @@ from django.conf import settings
 
 def show_toolbar(request):
     """
-    For showing django-debug-toolbar when running inside docker, for detail:
+    For showing django-debug-toolbar when running inside Docker, see:
     https://github.com/gnhuy91/til/blob/master/django/debug-toolbar-in-docker.md
     """
     if request.is_ajax():
