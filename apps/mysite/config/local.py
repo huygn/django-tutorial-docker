a597f03c80baf5ff56d733756fbc854fb17dfd71
Huy Giang <gnhuy91@gmail.com>
2016-04-02 00:03:14 +0700

    simple workaround for debug toolbar inside docker

--- apps/mysite/config/local.py
+++ apps/mysite/config/local.py
@@ -1,12 +1,17 @@
 """
 Configurations for local development.
 """
+import os
+
+from django.conf import settings
+
 
 def show_toolbar(request):
     """
-    For showing django-debug-toolbar when running inside docker.
-    credit: https://gist.github.com/douglasmiranda/9de51aaba14543851ca3
+    For showing django-debug-toolbar when running inside docker, for detail:
+    https://github.com/gnhuy91/til/blob/master/django/debug-toolbar-in-docker.md
     """
     if request.is_ajax():
         return False
-    return True
+
+    return bool(settings.DEBUG)
